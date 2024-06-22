c36bdefa105b64300d912d6d524163b257fc5c7a
Ağa Rzayev <rzayevaga.dev@gmail.com>
2024-06-22 21:09:56 +0400

    Add files via upload

---
+++ Ai-tech/modules/ping.py
@@ -0,0 +1,36 @@
+import random
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import IMG, OWNER_USERNAME, STICKER
+from Aitech import Rzayev
+from Aitech.database.chats import add_served_chat
+from Aitech.database.users import add_served_user
+from Aitech.modules.helpers import PNG_BTN
+
+
+@Rzayev.on_cmd("ping")
+async def ping(_, message: Message):
+    await message.reply_sticker(sticker=random.choice(STICKER))
+    start = datetime.now()
+    loda = await message.reply_photo(
+        photo=random.choice(IMG),
+        caption="ᴘɪɴɢɪɴɢ...",
+    )
+    try:
+        await message.delete()
+    except:
+        pass
+
+    ms = (datetime.now() - start).microseconds / 1000
+    await loda.edit_text(
+        text=f"Salam Beybi!!\n{Rzayev.name} Pong\n➥ `{ms}` ms\n\n<b>||❣️ ву [Rzayev](https://t.me/{OWNER_USERNAME}) ||</b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
+    if message.chat.type == ChatType.PRIVATE:
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_chat(message.chat.id)
