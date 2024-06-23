290b470ff70cce706365beceb86aa7688d0f0293
Ağa Rzayev <rzayevaga.dev@gmail.com>
2024-06-23 11:58:31 +0400

    Update config.py

--- config.py
+++ config.py
@@ -23,9 +23,8 @@ IMG = [
 
 # Random Stickers
 STICKER = [
-    "https://t.me/addstickers/TheCoin",
-    "https://t.me/addstickers/Webp_14",
-    "https://t.me/addstickers/Webp_14",
+    "CAACAgQAAxkBAAEQ245ljYcpjiUzNlnqZayXwYGXQdQUYgAC2Q8AAnsbSFJTlxo",
+    "CAACAgQAAxkBAAEQ245ljYcpjiUzNlnqZayXwYGXQdQUYgAC2Q8AAnsbSFJTlxo",
 ]
 
 
