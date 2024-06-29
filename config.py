b26e0a3b92dea2bbdf02080892ba54ec9da84a5f
Ağa Rzayev <rzayevaga.dev@gmail.com>
2024-06-29 23:08:18 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
+### ~ Aitech config : aichatbot
+
 from os import getenv
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-OWNER_ID = int(getenv("OWNER_ID", "1924693109"))
-STRING_SESSION = getenv("STRING_SESSION", "")
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", "")) # get my.telegram.org/apps
+API_HASH = getenv("API_HASH", "") # get my.telegram.org/apps
+OWNER_ID = int(getenv("OWNER_ID", "1924693109")) # Owner ID: get @MissRose_bot /info ...
+STRING_SESSION = getenv("STRING_SESSION", "") # Pyrogram v2 String Session: help @aiteknoloji
+MONGO_URL = getenv("MONGO_URL", "") # Free mongo db url: "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 SUPPORT_GRP = getenv("SUPPORT_GRP", "aitsupport")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "aitbots")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "codmastervip")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "codmastervip") # Owner username "@" without 
 
 # Random Start Images
 IMG = [
@@ -27,7 +29,7 @@ STICKER = [
     "https://graph.org/file/ff8fa5df03aecffd1b010.jpg",
 ]
 
-
+# Random Emojios
 EMOJIOS = [
     "😍",
     "🌹",
